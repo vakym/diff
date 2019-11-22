186d7b2cbb74dbc03946a67bae5c36e1b51e977d
vakym <aep@rifcom.ru>
2019-11-22 15:25:03 +0300

    черновик v2. Проходит не все тесты

--- Algebra.cs
+++ Algebra.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,68 +12,91 @@ namespace Reflection.Differentiation
     {
         public static Expression<Func<double, double>> Differentiate(Expression<Func<double, double>> function)
         {
-            var f = function.Body;
-            var x = Expression.Parameter(typeof(double), "x");
-            if (f is ConstantExpression) 
-                ///f(x) = c => f'(x) = 0
-                ///(x) => 0
-                return Expression.Lambda<Func<double, double>>(
-                    Expression.Constant(0d),x
-                    );
-            if(f is ParameterExpression)
-                ///f(x) = x => f'(x) = 1;
-                ///(x) => 1
-                return Expression.Lambda<Func<double, double>>(
-                    Expression.Constant(1d), x);
-            var listOfTerms = GetTerms(f as BinaryExpression);
-            var termsCount = listOfTerms.Count;
-            do
-            {
-                termsCount = listOfTerms.Count;
-                foreach (var term in listOfTerms)
-                {
-                    listOfTerms.AddRange(GetTerms(term as BinaryExpression));
-                }
-            } while (listOfTerms.Count != termsCount);
+            return  Expression.Lambda<Func<double,double>>(BuildSumExpression(Diff(function.Body)),x);
+        }
+
+        private static Queue<Expression> Diff(Expression expression)
+        {
+            var listOfTerms = GetAllTerms(expression);
             var diffListOfTerms = new Queue<Expression>();
             foreach (var term in listOfTerms)
             {
-                if(term is BinaryExpression)
+                switch (term.NodeType)
                 {
-                    diffListOfTerms.Enqueue(GetParametrDiff(term as BinaryExpression));
+                    case ExpressionType.Constant:
+                        diffListOfTerms.Enqueue(GetConstantDiff());
+                        break;
+                    case ExpressionType.Call:
+                        diffListOfTerms.Enqueue(GetMethodDiff(term as MethodCallExpression));
+                        break;
+                    default:
+                        diffListOfTerms.Enqueue(GetParametrDiff(term));
+                        break;
+
                 }
             }
-            return BuildLambda(diffListOfTerms,x);
+            return diffListOfTerms;
         }
-
-        private static Expression<Func<double,double>> BuildLambda(Queue<Expression> expressions, ParameterExpression x)
+        private static ParameterExpression x = Expression.Parameter(typeof(double), "z");
+        private static List<Expression> GetAllTerms(Expression expression)
+        {
+            var terms = new List<Expression>();
+            if (expression is BinaryExpression binary
+                && expression.NodeType == ExpressionType.Add)
+            {
+                if (binary.Left is BinaryExpression left)
+                {
+                    terms.AddRange(GetAllTerms(left));
+                }
+                else
+                {
+                    terms.Add(binary.Left);
+                }
+                if (binary.Right is BinaryExpression rigth)
+                {
+                    terms.AddRange(GetAllTerms(rigth));
+                }
+                else
+                {
+                    terms.Add(binary.Right);
+                }
+            }
+            else
+                terms.Add(expression);
+            return terms;
+        }
+        private static Expression BuildSumExpression(Queue<Expression> expressions)
         {
             if(expressions.Count == 1)
-                return Expression.Lambda<Func<double, double>>(expressions.Dequeue(), x);
+                return expressions.Dequeue();
 
             var left = expressions.Dequeue();
             Expression rigth;
-            while ((rigth = expressions.Dequeue())!=null)
+            while (expressions.Count!=0)
             {
+                rigth = expressions.Dequeue();
                 left = Expression.Add(left, rigth);
             }
-            return Expression.Lambda<Func<double, double>>(left, x);
-        }   
-        private static List<Expression> GetTerms(BinaryExpression binaryExpression)
-        {
-            return binaryExpression.NodeType == ExpressionType.Add 
-                ? new List<Expression>() { binaryExpression.Left, binaryExpression.Right } 
-                : new List<Expression>() { binaryExpression };
+            return left;
         }
-
-        private static Expression GetParametrDiff(BinaryExpression expression)
+        #region Rules of differentiation
+        //f(x) = c*x^y => f'(x) = c*y*x^y-1  c,y - constant
+        private static Expression GetParametrDiff(Expression expression)
         {
-            var x = Expression.Parameter(typeof(double), "x");
-            var funcData = GetFunctionProperties(expression);
-            return Expression.Multiply(
-                        Expression.Constant(funcData.Constant * funcData.Pow),
-                        Expression.Call(null,typeof(Math).GetMethod("Pow"),x,Expression.Constant((funcData.Pow - 1))));
-            
+            if (expression is ParameterExpression)
+                return Expression.Constant(1d);
+            else
+            {
+                var funcData = GetFunctionProperties(expression as BinaryExpression);
+                if (funcData.Constant == 0)
+                {
+                    funcData.Constant = 1;
+                }
+                return Expression.Multiply(
+                            Expression.Constant(funcData.Constant * funcData.Pow),
+                            Expression.Call(null, typeof(Math).GetMethod("Pow"), x, Expression.Constant((funcData.Pow - 1))));
+            }
+
             FunctionProperty GetFunctionProperties(BinaryExpression subExpression)
             {
                 var funcInfo = new FunctionProperty();
@@ -109,7 +133,43 @@ namespace Reflection.Differentiation
                 return funcInfo;
             }
         }
-       
+
+        //f(x) = c => f'(x) = 0 c - constant
+        private static Expression GetConstantDiff()
+        {
+            return Expression.Constant(0d);
+        }
+
+        //f(x) = sin(x) => f'(x) = cos(x)
+        //f(x) = sin(x^2) => f'(x) = sin(x^2)*2
+        private static Expression GetMethodDiff(MethodCallExpression term)
+        {
+            var method = GetMethod(term.Method);
+            if((term.Arguments.First().NodeType !=ExpressionType.Parameter))
+            {
+                var ex = BuildSumExpression(Diff(term.Arguments.First()));
+                var param = term.Arguments.First();
+                
+                return Expression.Multiply(Expression.Call(null, method, param), ex);
+            }
+            else
+            {
+                return Expression.Call(null, method, x);
+            }
+            MethodInfo GetMethod(MethodInfo currentMethod)
+            {
+                if(currentMethod.Name=="Sin")
+                {
+                    return typeof(Math).GetMethod("Cos");
+                }
+                if(currentMethod.Name == "Cos")
+                {
+                    return typeof(Math).GetMethod("Sin");
+                }
+                throw new NotImplementedException($"Differentiation for the {currentMethod.Name} method has not yet been implemented");
+            }
+        }
+        #endregion
         private static bool IsTheEndNode(Expression expression)
         {
             return expression is ConstantExpression || expression is ParameterExpression;
